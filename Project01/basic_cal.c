613c9a629f23e3fabe6f55cdd06bb79f995c4ecd
Mukesh Kumar <mukeshkumar916241@gmail.com>
2025-11-16 16:30:04 +0530

    Update basic_cal.c

--- Project01/basic_cal.c
+++ Project01/basic_cal.c
@@ -1,46 +1,63 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
-int main()
+int main(void)
 {
-    int num1,num2;
+    double num1, num2;
     char operator;
-    printf(" Basic calculator (Addition, Subtraction, Multiplication & Division)\n");
-    printf("Enter 1st Number:\t ");
-    scanf("%d",&num1);
+    const double EPS = 1e-12; 
+    int choice = 0;
+    int running = 1;
 
-    printf("Enter 2nd Number:\t ");
-    scanf("%d",&num2);
+    printf("Basic calculator (Addition, Subtraction, Multiplication & Division)\n");
 
-    printf(" + for addition\n - for subtraction\n * for multiplication\n / for division\n Enter Operator sign : ");
-    scanf(" %c",&operator);
-    
-    switch(operator)
-        {
-        case '+':
-            printf(" Addition of %d %c %d = %d",num1,operator,num2,num1+num2);
-            break;
-        case '-':
-            printf(" Subtraction of %d %c %d = %d",num1,operator,num2,num1-num2);
-            break;
-        case '*':
-            printf(" Multiplication of %d %c %d = %d",num1,operator,num2,num1*num2);
-            break;
-        case '/':
-            if (num2==0)
-            {
-                 printf("Error: Cannot divide by zero.");
-            }
-            else
-            {
-                printf(" Division of %d %c %d = %.2f",num1,operator,num2,(float) num1/num2);
-            }
-            break;
-        default :
-            printf("Invalid input ");
-            break;
+    while (running) {
+        printf("\nMenu:\n");
+        printf(" 1) Calculate\n");
+        printf(" 2) Exit\n");
+        printf("Choose option: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("Enter 1st Number:\t ");
+                scanf("%lf", &num1);
+                printf("Enter 2nd Number:\t ");
+                scanf("%lf", &num2);
+
+                printf(" + for addition\n - for subtraction\n * for multiplication\n / for division\n Enter Operator sign : ");
+                scanf(" %c", &operator);
+
+                switch (operator) {
+                    case '+':
+                        printf(" Addition of %.6g %c %.6g = %.6g\n", num1, operator, num2, num1 + num2);
+                        break;
+                    case '-':
+                        printf(" Subtraction of %.6g %c %.6g = %.6g\n", num1, operator, num2, num1 - num2);
+                        break;
+                    case '*':
+                        printf(" Multiplication of %.6g %c %.6g = %.6g\n", num1, operator, num2, num1 * num2);
+                        break;
+                    case '/':
+                        if (fabs(num2) < EPS) {
+                            printf("Error: Cannot divide by zero (or value too close to zero).\n");
+                        } else {
+                            printf(" Division of %.6g %c %.6g = %.6g\n", num1, operator, num2, num1 / num2);
+                        }
+                        break;
+                    default:
+                        printf("Invalid operator '%c'. Use +, -, * or /.\n", operator);
+                        break;
+                }
+                break;
+            case 2:
+                running = 0;
+                break;
+            default:
+                printf("Invalid menu option. Please enter 1 or 2.\n");
+                break;
         }
+    }
 
-
+    printf("Exiting calculator. Goodbye.\n");
     return 0;
-
 }
